69a63285ac64eae91c6d95f82c47a0313ce4663d
dvprasad45 <dvprasad45@gmail.com>
2025-07-14 13:06:22 +0530

    spring boot base project setup

---
+++ src/test/java/com/hcl/usermanagement/UserManagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagement2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
